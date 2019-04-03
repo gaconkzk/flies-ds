412c740a797538fb8c7aa98d859afa05eca7aa94
gaconkzk <gaconkzk@gmail.com>
2019-04-03 16:19:05 +0700

    use vue-cli

--- src/main.js
+++ src/main.js
@@ -14,10 +14,7 @@ Vue.use(Meta)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: "#app",
   router,
-  template: "<App/>",
-  components: { App },
-})
+  render: h => h(App),
+}).$mount("#app")
